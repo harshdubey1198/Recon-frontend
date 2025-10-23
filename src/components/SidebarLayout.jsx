a45346ab687ddf598072e209be7cae2cbde6112b
HARSH DUBEY <amanad08052@gmail.com>
2025-10-23 11:24:12 +0530

    refresh to same tab done

--- src/components/SidebarLayout.jsx
+++ src/components/SidebarLayout.jsx
@@ -15,7 +15,8 @@ import UserCategories from "../pages/UserCategories";
 
 
 export default function SidebarLayout() {
-    const [activeItem, setActiveItem] = useState("Dashboard");
+    // const [activeItem, setActiveItem] = useState("Dashboard");
+    const [activeItem, setActiveItem] = useState(localStorage.getItem("activeTab") || "Dashboard");
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const navigate = useNavigate();
@@ -113,6 +114,7 @@ const menuItems = [
                   <button
                     onClick={() => {
                       setActiveItem(item.name);
+                      localStorage.setItem("activeTab", item.name);
                       setIsMobileMenuOpen(false);
                     }}
                     className={`
