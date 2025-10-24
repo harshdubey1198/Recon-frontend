4cc346984bb0af94aafda0bb4658a998b13a31b7
HARSH DUBEY <amanad08052@gmail.com>
2025-10-24 11:19:07 +0530

    toast added for the error case of the create news article

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,20 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 // import "./App.css";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          pauseOnHover
+          closeOnClick
+          draggable
+        />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
