355ae29b69761074b9a2f6f981e42faf4e9a35d5
satyam dubey <100850613+Satyamdubey6367@users.noreply.github.com>
2025-11-13 18:34:18 +0530

    push it

--- src/pages/CreateNews.jsx
+++ src/pages/CreateNews.jsx
@@ -1204,7 +1204,7 @@ const resetForm = () => {
                         {
                           name: 'paragraph',
                           items: ['NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock',]
-                        },
+                        },
                         { name: 'styles', items: ['Styles','Format','Font','FontSize'] },
                         { name: 'colors', items: ['TextColor','BGColor'] },
                         { name: 'tools', items: ['Maximize'] },
